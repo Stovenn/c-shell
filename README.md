11df9028906c4e1f86372cb8329145ca9f6f6b93
Stovenn <stvndev93@gmail.com>
2025-09-10 07:44:45 +0200

    simple REPL + README

---
+++ README.md
@@ -0,0 +1,4 @@
+# C-shell
+
+A POSIX compliant shell written in C.
+Only for learning purposes and get familiar with the C syntax.
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  while (1) {
+    // Flush after every printf
+    setbuf(stdout, NULL);
+
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    // removing trailing newline
+    input[strlen(input) -1] = '\0';
+
+    if (strcmp(input, "exit 0") == 0) {
+      break;
+    }
+
+    printf("%s: command not found\n", input);
+  }
+  return 0;
+}
